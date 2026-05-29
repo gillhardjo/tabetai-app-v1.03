51519127508285749112314b86e96c0ff0b6fc76
Gill.Hardjo <gill.hardjo@gmail.com>
2026-05-29 20:59:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,14 @@ const firebaseConfig = {
   measurementId: "G-6189BCY5YH"
 };
 
-// Inisialisasi Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 // --- INITIAL SHARED DATA ---
 const ADMIN_CREDENTIALS = {
-  username: 'gillhardjo',
-  phone: '081285557779'
+  username: 'admin',
+  phone: '2131'
 };
 
 // Image URLs & WhatsApp
